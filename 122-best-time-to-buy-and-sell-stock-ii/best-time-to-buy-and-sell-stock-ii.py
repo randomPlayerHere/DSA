e276e4a7a3b6916fc4ff6fb76128b2f5958d94bc
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-03-15 02:59:15 +0530

    Time: 11 ms (9.00%) | Memory: 20.4 MB (41.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,14 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        def tabulation():
-            dp = [[0]*2 for _ in range(n+1)]
-            dp[n][0] = dp[n][1] = 0
+        # def tabulation():
+        #     dp = [[0]*2 for _ in range(n+1)]
+            # dp[n][0] = dp[n][1] = 0
+            # for i in range(n-1,-1,-1):
+            #     for j in range(2):
+            #         if j==0:
+            #             dp[i][j] = max(-prices[i] + dp[i+1][1] ,dp[i+1][0])
+            #         else:
+            #             dp[i][j] = max(prices[i] + dp[i+1][0] ,dp[i+1][1])
+            # return dp[0][0]
+        # return tabulation()
+        def sop():
+            right_0 = 0
+            right_1 = 0
             for i in range(n-1,-1,-1):
                 for j in range(2):
                     if j==0:
-                        dp[i][j] = max(-prices[i] + dp[i+1][1] ,dp[i+1][0])
+                        right_0 = max(-prices[i] + right_1 ,right_0)
                     else:
-                        dp[i][j] = max(prices[i] + dp[i+1][0] ,dp[i+1][1])
-            return dp[0][0]
-        return tabulation()
+                        right_1 = max(prices[i] + right_0 ,right_1)
+            return right_0
+        return sop()

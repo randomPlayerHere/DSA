985968799847196fdb488053e511a848484dec05
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-03-15 02:56:16 +0530

    Time: 19 ms (6.73%) | Memory: 23.4 MB (5.07%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i = 0
-        lo = prices[0]
-        hi = prices[0]
-        profit = 0
         n = len(prices)
-        while i < n-1:
-            while i < n-1 and prices[i] >= prices[i+1]:
-                i += 1
-            lo = prices[i]
-            while i < n-1 and prices[i] <= prices[i+1]:
-                i += 1
-            hi = prices[i]
-            profit += hi - lo
-        return profit
+        def tabulation():
+            dp = [[0]*2 for _ in range(n+1)]
+            dp[n][0] = dp[n][1] = 0
+            for i in range(n-1,-1,-1):
+                for j in range(2):
+                    if j==0:
+                        dp[i][j] = max(-prices[i] + dp[i+1][1] ,dp[i+1][0])
+                    else:
+                        dp[i][j] = max(prices[i] + dp[i+1][0] ,dp[i+1][1])
+            return dp[0][0]
+        return tabulation()

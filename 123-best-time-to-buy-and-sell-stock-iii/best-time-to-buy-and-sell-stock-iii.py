3d96c166f6beadd1c52a370aebe18d2ef24fbbde
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-03-16 07:19:40 +0530

    Time: 359 ms (28.74%) | Memory: 50 MB (23.60%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -23,5 +23,20 @@ class Solution:
                             profit = max(prices[i] + dp[i+1][0][k-1], dp[i+1][1][k])
                         dp[i][j][k] = profit
             return dp[0][0][2]
-        return tabulation()
+        # return tabulation()
+
+        def space_optimized():
+            right_i = [[0 for _ in range(3)] for _ in range(2)]
+            curr_i = [[0 for _ in range(3)] for _ in range(2)]
+            for i in range(n-1, -1, -1):
+                for j in range(2):
+                    for k in range(1,3):
+                        if j ==0:
+                            curr_i[j][k]= max(-prices[i] + right_i[1][k], right_i[0][k])
+                        else:
+                            curr_i[j][k]= max(prices[i] + right_i[0][k-1], right_i[1][k])
+                right_i = curr_i
+            return right_i[0][2]
+        return space_optimized()
+
 

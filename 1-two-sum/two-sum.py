22833c468d6a675a76581d63e0c59b8f43378f72
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-04-25 09:18:39 +0530

    Time: 3 ms (52.44%) | Memory: 20.5 MB (21.60%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        hash_map = dict()
+        for i in range(n):
+            needed = target-nums[i]
+            if hash_map.get(needed,-1) != -1:
+                return [hash_map[needed],i]
+            else:
+                hash_map[nums[i]] = i
+        
+            

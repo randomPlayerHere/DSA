e8577d6f6e8cf87a8cfa1894b0ed2fa74a39b73b
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-05-21 05:29:39 +0530

    Time: 112 ms (75.21%) | Memory: 113.2 MB (35.36%) - LeetSync

---
+++ 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.py
@@ -0,0 +1,36 @@
+from collections import defaultdict, deque
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        visited = set()
+        restricted = set(restricted)
+        def edges_to_adj_list(n, edges):
+            graph = defaultdict(list)
+            for n1, n2 in edges:
+                graph[n1].append(n2)
+                graph[n2].append(n1)
+            return graph
+        graph = edges_to_adj_list(n, edges)
+            
+        def dfs(node):
+            if node in restricted:
+                return
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        def bfs(root):
+            que = deque([root])
+            visited.add(root)
+            while que:
+                node = que.popleft()
+                for nei in graph[node]:
+                    if nei in restricted:
+                        continue
+                    elif nei not in visited:
+                        visited.add(nei)
+                        que.append(nei)
+        
+        bfs(0)
+        return len(visited)
+

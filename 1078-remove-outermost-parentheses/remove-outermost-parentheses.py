04f875d9e7dd9af2af998dfb9d2652c943e183e1
Khushal Nirwal <104513074+randomPlayerHere@users.noreply.github.com>
2026-06-01 06:21:04 +0530

    Time: 3 ms (68.84%) | Memory: 19.3 MB (31.16%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -6,8 +6,8 @@ class Solution:
         for i in range(len(s)):
             count+= 1 if s[i]=="(" else -1
             if count==0:
-                primit.append(s[prev_cut:i+1])
+                primit.append(s[prev_cut+1:i+1-1])
                 prev_cut = i+1
-        primit = [s[1:-1] for s in primit]
+        # primit = [s[1:-1] for s in primit]
         return "".join(primit)
         
